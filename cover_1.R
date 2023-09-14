7ceb97e875ec1e2b69af24d54a30c15ed2a7fcd4
Arthur Ferreira <arthur_sf@icloud.com>
2023-09-14 19:03:40 -0300

    Commit.

--- cover_1.R
+++ cover_1.R
@@ -30,30 +30,31 @@ for(i in 1:2){
   }
   phrases <- phrases[!duplicated(phrases)]
   texto <- paste0(phrases, collapse = " ")
-  rectangleWidth <- W
+  rectangleWidth <- W + 2 # to simulate a text justified
   s <- texto
   n <- nchar(s)
   for(i in n:1) {
     wrappeds <- paste0(strwrap(s, i), collapse = "\n")
     if(strwidth(wrappeds) < rectangleWidth) break
   }
+  wrappeds <- paste(wrappeds, wrappeds)
   textHeight <- strheight(wrappeds)
-  text(W/2, H/2, labels = wrappeds, col = "gray35")
+  text(W/2, H/2, labels = wrappeds, col = "gray25")
   
   # author name
-  rect(xleft = 0,  ybottom = H - 0.8, xright = W, ytop = H, col = "black")
-  text(x = W, y = H - 0.4, labels = substitute(paste(bold("Arthur de Sá Ferreira"))), pos = 2, cex = 2, col = "white")
+#  rect(xleft = 0 - 10,  ybottom = H - 1, xright = W + 10, ytop = H + 1, col = "black")
+  text(x = W, y = H, labels = substitute(paste(bold("Arthur de Sá Ferreira"))), pos = 2, cex = 2, col = "white")
   
   # book title
-  rect(xleft = 0,  ybottom = H/2 + 2.0 - 1.16, xright = W, ytop = H/2 + 2.0 + 1, col = "black")
-  text(x = W, y = H/2 + 2.0, labels = substitute(paste(bold("Ciência com    "))), pos = 2, cex = 5, col = "yellow")
-  text(x = W, y = H/2 + 2.0, labels = substitute(paste(bold("R"))), pos = 2, cex = 5, col = "#3363B0")
+#  rect(xleft = 0 - 10,  ybottom = H - 1 - 1, xright = W + 10, ytop = H - 1 + 1, col = "black")
+  text(x = W, y = H/2 - 1 - 1, labels = substitute(paste(bold("Ciência com   "))), pos = 2, cex = 6.5, col = "yellow")
+  text(x = W, y = H/2 - 1 - 1, labels = substitute(paste(bold("R"))), pos = 2, cex = 6.5, col = "#3363B0")
   
   # book subtitle
-  text(x = W, y = H/2 + 1.25, labels = substitute(paste(italic("Perguntas e respostas para pesquisadores e analistas de dados"))), pos = 2, cex = 1.5, col = "white")
+  text(x = W, y = H/2 - 2.75, labels = substitute(paste(italic("Perguntas e respostas para pesquisadores e analistas de dados"))), pos = 2, cex = 1.5, col = "white")
 
   # year
-  rect(xleft = 0,  ybottom = -0.5, xright = W, ytop = 0.3, col = "black")
+#  rect(xleft = 0 - 10,  ybottom = -0.5, xright = W + 10, ytop = 0.3, col = "black")
   text(x = W/2, y = -0.2, labels = format(Sys.Date(), "%Y"), adj = c(0,0), cex = 1, col = "white")
   
   dev.off()
