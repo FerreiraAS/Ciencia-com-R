bf634d9df0b4e91dc6e1e6fcebe8951cfc502097
Arthur Ferreira <arthur_sf@icloud.com>
2023-09-15 16:58:49 -0300

    Commit.

--- cover_1.R
+++ cover_1.R
@@ -43,19 +43,19 @@ for(i in 1:2){
   
   # author name
 #  rect(xleft = 0 - 10,  ybottom = H - 1, xright = W + 10, ytop = H + 1, col = "black")
-  text(x = W, y = H, labels = substitute(paste(bold("Arthur de Sá Ferreira"))), pos = 2, cex = 2, col = "white")
+  text(x = W, y = H - 0.5, labels = substitute(paste(bold("Arthur de Sá Ferreira"))), pos = 2, cex = 2, col = "white")
   
   # book title
 #  rect(xleft = 0 - 10,  ybottom = H - 1 - 1, xright = W + 10, ytop = H - 1 + 1, col = "black")
-  text(x = W, y = H/2 - 1 - 1, labels = substitute(paste(bold("Ciência com   "))), pos = 2, cex = 6.5, col = "yellow")
-  text(x = W, y = H/2 - 1 - 1, labels = substitute(paste(bold("R"))), pos = 2, cex = 6.5, col = "#3363B0")
+  text(x = W, y = H/2 + 2 - 1, labels = substitute(paste(bold("Ciência com   "))), pos = 2, cex = 6.5, col = "yellow")
+  text(x = W, y = H/2 + 2 - 1, labels = substitute(paste(bold("R"))), pos = 2, cex = 6.5, col = "#3363B0")
   
   # book subtitle
-  text(x = W, y = H/2 - 2.75, labels = substitute(paste(italic("Perguntas e respostas para pesquisadores e analistas de dados"))), pos = 2, cex = 1.5, col = "white")
+  text(x = W, y = H/2 + 3 - 2.75, labels = substitute(paste(italic("Perguntas e respostas para pesquisadores e analistas de dados"))), pos = 2, cex = 1.5, col = "white")
 
   # year
 #  rect(xleft = 0 - 10,  ybottom = -0.5, xright = W + 10, ytop = 0.3, col = "black")
-  text(x = W/2, y = -0.2, labels = format(Sys.Date(), "%Y"), adj = c(0,0), cex = 1.5, col = "white")
+  text(x = W/2, y = 0, labels = format(Sys.Date(), "%Y"), adj = c(0,0), cex = 1.5, col = "white")
   
   dev.off()
 }
