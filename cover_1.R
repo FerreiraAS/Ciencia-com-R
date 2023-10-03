14f74c17f34b1c3d707eb0b1fa314d62682c1586
Arthur Ferreira <arthur_sf@icloud.com>
2023-10-03 11:47:21 -0300

    Commit.

--- cover_1.R
+++ cover_1.R
@@ -42,11 +42,9 @@ for(i in 1:2){
   text(W/2, H/2, labels = wrappeds, col = "gray25")
   
   # author name
-#  rect(xleft = 0 - 10,  ybottom = H - 1, xright = W + 10, ytop = H + 1, col = "black")
   text(x = W, y = H - 0.5, labels = substitute(paste(bold("Arthur de Sá Ferreira"))), pos = 2, cex = 2, col = "white")
   
   # book title
-#  rect(xleft = 0 - 10,  ybottom = H - 1 - 1, xright = W + 10, ytop = H - 1 + 1, col = "black")
   text(x = W, y = H/2 + 2 - 1, labels = substitute(paste(bold("Ciência com   "))), pos = 2, cex = 6.5, col = "yellow")
   text(x = W, y = H/2 + 2 - 1, labels = substitute(paste(bold("R"))), pos = 2, cex = 6.5, col = "#3363B0")
   
@@ -54,7 +52,6 @@ for(i in 1:2){
   text(x = W, y = H/2 + 3 - 2.75, labels = substitute(paste(italic("Perguntas e respostas para pesquisadores e analistas de dados"))), pos = 2, cex = 1.5, col = "white")
 
   # year
-#  rect(xleft = 0 - 10,  ybottom = -0.5, xright = W + 10, ytop = 0.3, col = "black")
   text(x = W/2, y = 0, labels = format(Sys.Date(), "%Y"), adj = c(0,0), cex = 1.5, col = "white")
   
   dev.off()
---
+++ cover_2.R
@@ -0,0 +1,34 @@
+W <- 8.27
+H <- 11.69
+
+for(i in 1:2){
+  
+  if(i == 1){
+    grDevices::png(file = "Cover_2.png",
+                   width = W,
+                   height = H,
+                   units = "in",
+                   res = 300)
+  } else {
+    grDevices::pdf(file = "Cover_2.pdf",
+                   width = W,
+                   height = H)
+  }
+  par(mar = c(0, 0, 0, 0), oma = c(0, 0, 0, 0), bg = "white")
+  plot(1, type = "n", xlab = "", ylab = "", xlim = c(0,W), ylim = c(0,H))
+  
+  # author name
+  text(x = W, y = H - 0.5, labels = substitute(paste(bold("Arthur de Sá Ferreira"))), pos = 2, cex = 2, col = "black")
+  
+  # book title
+  text(x = W, y = H/2 + 2 - 1, labels = substitute(paste(bold("Ciência com   "))), pos = 2, cex = 6.5, col = "black")
+  text(x = W, y = H/2 + 2 - 1, labels = substitute(paste(bold("R"))), pos = 2, cex = 6.5, col = "black")
+  
+  # book subtitle
+  text(x = W, y = H/2 + 3 - 2.75, labels = substitute(paste(italic("Perguntas e respostas para pesquisadores e analistas de dados"))), pos = 2, cex = 1.5, col = "black")
+
+  # year
+  text(x = W/2, y = 0, labels = format(Sys.Date(), "%Y"), adj = c(0,0), cex = 1.5, col = "black")
+  
+  dev.off()
+}
